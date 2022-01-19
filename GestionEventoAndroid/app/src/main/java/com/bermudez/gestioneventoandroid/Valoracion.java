7d05fb337ca8bfc45d6c44856ff518621a2722de
albferjim <albferjim@gmail.com>
2022-01-19 09:28:55 +0100

    Comentario y Valoracion

---
+++ GestionEventoAndroid/app/src/main/java/com/bermudez/gestioneventoandroid/Valoracion.java
@@ -0,0 +1,14 @@
+package com.bermudez.gestioneventoandroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Valoracion extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_valoracion);
+    }
+}
