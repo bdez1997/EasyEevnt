e01f2f7e865aef06c9ef577cf56656b9483ef871
IreneCejas <ireneline25@gmail.com>
2022-01-26 11:09:11 +0100

    PersonaEvento
    HashCode y equals

---
+++ GestionEventoAndroid/app/src/main/java/com/bermudez/gestioneventoandroid/models/Persona_Evento.java
@@ -0,0 +1,50 @@
+package com.bermudez.gestioneventoandroid.models;
+
+import java.util.Objects;
+
+public class Persona_Evento implements IMaxCaracteres{
+
+    private Persona oPersona;
+    private Evento oEvento;
+    private String sValoracion;
+
+    public String getsValoracion() {
+        return sValoracion;
+    }
+
+    public void setsValoracion(String sValoracion) {
+        if (sValoracion.length() > IMINIMO && sValoracion.length() < IMAXDESCRIPCION) {
+            this.sValoracion = sValoracion;
+        }
+    }
+
+    public Persona_Evento(Persona oPersona, Evento oEvento) {
+        oPersona.setsDni(null);
+        oEvento.setIdEvento(0);
+    }
+
+    public Persona_Evento(Persona oPersona, Evento oEvento, String sValoracion) {
+        oPersona.setsDni(null);
+        oEvento.setIdEvento(0);
+        setsValoracion(sValoracion);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(oEvento, oPersona, sValoracion);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Persona_Evento other = (Persona_Evento) obj;
+        return Objects.equals(oEvento, other.oEvento) && Objects.equals(oPersona, other.oPersona)
+                && Objects.equals(sValoracion, other.sValoracion);
+    }
+
+}
