bf77b0f5c7f1e027051a245ac5841d662b40e028
IreneCejas <ireneline25@gmail.com>
2022-01-31 10:04:30 +0100

    Mostrar evento
    Crear método para obtener lista de eventos

--- GestionEventoAndroid/app/src/main/java/com/bermudez/gestioneventoandroid/fragments/PrincipalFragment.java
+++ GestionEventoAndroid/app/src/main/java/com/bermudez/gestioneventoandroid/fragments/PrincipalFragment.java
@@ -7,14 +7,22 @@ import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.android.volley.Request;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.bermudez.gestioneventoandroid.InfoEvento;
 import com.bermudez.gestioneventoandroid.R;
 import com.bermudez.gestioneventoandroid.controller.EventosAdapter;
 import com.bermudez.gestioneventoandroid.controller.Store;
+import com.bermudez.gestioneventoandroid.models.Evento;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class PrincipalFragment extends Fragment {
 
@@ -53,6 +61,43 @@ public class PrincipalFragment extends Fragment {
             //((Activity) getActivity()).overridePendingTransition(0, 0);
         });
     }
+
+    private void obtenerTodos() {
+        String url = "http://proyectogestioneventos.atwebpages.com/php/get-evento.php";
+        Volley.newRequestQueue(getContext()).add(new StringRequest(Request.Method.GET, url,
+                s -> {
+                    try {
+                        JSONObject json = new JSONObject(s);
+                        jsonToEvent(json);
+
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                },
+                r ->{
+
+                }
+
+        ));
+    }
+
+    private void jsonToEvent(JSONObject json) throws JSONException {
+
+        int id = json.getInt("idEvento");
+        String nombre = json.getString("Nombre");
+        String fechaIni = json.getString("FechaIni");
+        String fechaFin = json.getString("FechaFin");
+        int aforo = json.getInt("Aforo");
+        String descripcion = json.getString("Descripcion");
+        //String imagen = json.getString("Imagen");
+        String direccion = json.getString("Direccion");
+
+        Evento jsonEvento = new Evento(id,nombre, fechaIni, fechaFin, aforo, descripcion, direccion);
+        Store.miEvento = jsonEvento;
+        Log.i("MI USUARIO", Store.miEvento.toString());
+    }
+
+
 
 
 }
