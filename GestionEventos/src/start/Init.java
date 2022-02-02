89b6d71ecbab144fdf930570979a165c9bc34f57
Antonio José <bdez_1997@live.com>
2022-02-02 14:46:30 +0100

    {wip

--- GestionEventos/src/start/Init.java
+++ GestionEventos/src/start/Init.java
@@ -6,7 +6,7 @@ public class Init {
 
 	public static void main(String[] args) {
 		
-		controller.CtrlEvento.stringToEvento("/proyectogestioneventos.atwebpages.com/php/get-evento.php");
+		controller.CtrlEvento.stringToEvento("http://proyectogestioneventos.atwebpages.com/php/get-evento.php");
 
 		//new FrmPrincipal();
 		
