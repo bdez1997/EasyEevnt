215bf676b7e35314826d5f077cdec705802293a3
Antonio José <bdez_1997@live.com>
2022-02-08 13:45:05 +0100

    {wip

--- GestionEventoAndroid/app/src/main/java/com/bermudez/gestioneventoandroid/RegistroActivity.java
+++ GestionEventoAndroid/app/src/main/java/com/bermudez/gestioneventoandroid/RegistroActivity.java
@@ -2,7 +2,10 @@ package com.bermudez.gestioneventoandroid;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -14,28 +17,45 @@ import com.android.volley.toolbox.Volley;
 public class RegistroActivity extends AppCompatActivity {
 
     EditText txtDni, txtNombre, txtApellidos, txtUser, txtPass, txtTelefono, txtEmail, txtInformacion;
-
-
+    Spinner sPinnerRoles;
+    Button btnRegistro;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro);
 
-        findViewById(R.id.btnRegistro).setOnClickListener(e ->{
+
+        sPinnerRoles = (Spinner) findViewById(R.id.spinnerRoles);
+        txtDni = (EditText)findViewById(R.id.txtDni);
+        txtNombre = findViewById(R.id.txtNombre);
+        txtApellidos = findViewById(R.id.txtApellidos);
+        txtUser = findViewById(R.id.txtUser);
+        txtPass = findViewById(R.id.txtPass);
+        txtTelefono =  findViewById(R.id.txtTelefono);
+        txtEmail = findViewById(R.id.txtEmail);
+        txtInformacion = findViewById(R.id.txtInformacion);
+
+           findViewById(R.id.btnRegistro).setOnClickListener(e ->{
             registrarUsuario();
         });
 
     }
 
     private void registrarUsuario() {
-        String sUrl = "proyectogestioneventos.atwebpages.com/php/ins-persona.php";
-        Volley.newRequestQueue(this).add(new StringRequest(Request.Method.GET, sUrl,
+
+
+        String sUrl = "http://proyectogestioneventos.atwebpages.com/php/ins-persona.php?dni="+txtDni.getText()+"&nombre="+txtNombre.getText()+
+                "&apellidos="+txtApellidos.getText()+"&username="+txtUser.getText()+"&password="+txtPass.getText()+"&correo="+txtEmail.getText()+"&telefono="+txtTelefono.getText()+
+                "&rol="+sPinnerRoles.getSelectedItem().toString() +"&informacion="+txtInformacion.getText();
+        String sResultado=sUrl.replace(" ","%20");
+        Volley.newRequestQueue(this).add(new StringRequest(Request.Method.GET, sResultado,
                 s -> {
-                    
+                    Toast.makeText(getApplicationContext(), "Usuraio registrado con éxito", Toast.LENGTH_SHORT).show();
+                    finish();
                 },
 
                 r-> {
-
+                    Toast.makeText(getApplicationContext(), "Algo ha ido mal", Toast.LENGTH_SHORT).show();
                 }
         ));
     }
