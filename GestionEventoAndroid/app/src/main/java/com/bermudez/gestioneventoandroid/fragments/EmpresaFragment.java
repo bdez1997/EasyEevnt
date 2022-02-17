5ee55ea8b9be458ccd89f05d2dbdbfe2221fd0de
IreneCejas <ireneline25@gmail.com>
2022-02-17 21:19:36 +0100

    EmpresaEvento
    Creación empresa evento

---
+++ GestionEventoAndroid/app/src/main/java/com/bermudez/gestioneventoandroid/fragments/EmpresaFragment.java
@@ -0,0 +1,24 @@
+package com.bermudez.gestioneventoandroid.fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.bermudez.gestioneventoandroid.R;
+
+
+public class EmpresaFragment extends Fragment {
+
+    View view;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_empresa, container, false);
+    }
+}
